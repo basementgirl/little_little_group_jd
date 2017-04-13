195bf98975dfb58c1bd628273965124ade2a5af2
rongrong.li <basement_girl@163.com>
2017-04-13 22:37:59 +0800

    get用户总特征

--- user_feature.py
+++ user_feature.py
@@ -1,5 +1,13 @@
 import pandas as pd
 
-user_feature_type='jdata_sam/user_feature1'
+user_feature_type='jdata_sam/user_feature_type.csv'
+user_feature_cate='jdata_sam/user_feature_cate.csv'
+user_feature_all='jdata_sam/user_feature_all.csv'
 
-df_user=pd.read_csv(user_file,encoding='gbk')
+df_user_feature_type=pd.read_csv(user_feature_type)
+df_user_feature_cate=pd.read_csv(user_feature_cate)
+df_user_feature_cate=df_user_feature_cate.drop(['age','sex','user_lv_cd','user_reg_tm'],axis=1)
+
+df_user_feature=pd.merge(df_user_feature_type,df_user_feature_cate,on='user_id')
+df_user_feature.to_csv(user_feature_all,index=False)
+
