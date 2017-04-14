d2673a572282b240f5bb989db792cee9755f50d7
rongrong.li <basement_girl@163.com>
2017-04-14 17:14:20 +0800

    get item all feature successfully

--- item_feature_comment.py
+++ item_feature_comment.py
@@ -2,21 +2,15 @@ import pandas as pd
 
 comment_file='jdata_ori/JData_Comment.csv'
 product_file='jdata_ori/JData_Product.csv'
-item_feature_commit='jdata_sam/item_feature_commit.csv'
-
-
-def bad_rate_counter(group):
-    avg_bad_com=group['bad_comment_rate'].mean()
-    group['avg_bad_com']=avg_bad_com
-    return group[['sku_id','avg_bad_com']]
+item_feature_comment='jdata_sam/item_feature_comment.csv'
 
 
 def get_item_com_feature():
     df_com=pd.read_csv(comment_file)
-    df_com=df_com.groupby(['sku_id'], as_index=False).apply(bad_rate_counter)
+    df_com=df_com[['sku_id','bad_comment_rate']]
+    df_com=df_com.groupby(['sku_id'], as_index=False).mean()
     df_com = df_com.drop_duplicates()
     return df_com
-
 
 def get_item_basic_feature():
     df_product=pd.read_csv(product_file)
@@ -28,5 +22,5 @@ if __name__ == "__main__":
     item_commit=get_item_com_feature()
 
     product_feature=pd.merge(item_base,item_commit,on=['sku_id'])
-    product_feature.to_csv(item_feature_commit,index=False)
+    product_feature.to_csv(item_feature_comment,index=False)
 
