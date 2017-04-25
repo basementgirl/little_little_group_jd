28860cc38b63ea6937b214338e9cdf20f2622775
rongrong.li <basement_girl@163.com>
2017-04-25 22:19:12 +0800

    无大的改变

--- total_cycle_ui_flag.py
+++ total_cycle_ui_flag.py
@@ -4,9 +4,9 @@ from ui_flag import get_ui_flag_each_time
 
 
 #控制得到60次训练集flag的总循环
-j = 1
-firstDay = date(2016, 3, 11)
-while j < 61:
+j = 2
+firstDay = date(2016, 2, 12)
+while j < 3:
     ui_flag = get_ui_flag_each_time(j,firstDay)
     firstDay = firstDay + timedelta(1)
     print('di %d ge training set is done!' % j)
--- ui_feature_and_flag.py
+++ ui_feature_and_flag.py
@@ -1,14 +1,16 @@
 import pandas as pd
 
 
-ui_feature_file='ui_feature_and_flag/train_set_1_ui_feature.csv'
-ui_flag_file='ui_feature_and_flag/train_set_1_ui_flag.csv'
+ui_feature_file='ui_feature_and_flag/train_set_2_ui_feature.csv'
+ui_flag_file='ui_feature_and_flag/train_set_2_ui_flag.csv'
 
 ui_feature=pd.read_csv(ui_feature_file)
 ui_flag=pd.read_csv(ui_flag_file)
 
 ui_train_set=pd.merge(ui_feature,ui_flag,on=['user_id','sku_id'],how='left')
-_ = ui_train_set.fillna(0,inplace=True)
-ui_train_set.to_csv('ui_feature_and_flag/ui_train_set_1.csv',index=False)
+#_ = ui_train_set.fillna(0,inplace=True)
+#ui_train_set=ui_train_set.dropna()
+#ui_train_set=ui_train_set.drop('buy_or_not',axis=1)
+ui_train_set.to_csv('ui_feature_and_flag/simple_version_ui_train_set_2.csv',index=False)
 
 
