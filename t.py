31a2f7a3692967af49de3407de51cb6b181e17d3
feifan.wang <feifan.wang@tendcloud.com>
2017-05-07 18:33:36 +0800

    delete some file which I think useless

--- t.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-df=pd.read_csv('ui_feature_and_flag/ui_train_set_5.csv')
-df[['user_id','sku_id','buy_or_not']].to_csv('ui_feature_and_flag/test_flag.csv',index=False)
-df=df.drop('buy_or_not',axis=1)
-df.to_csv('ui_feature_and_flag/test_feature.csv',index=False)
